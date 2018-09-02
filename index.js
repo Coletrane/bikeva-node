d2aef11e150522bccb782a0f620e23dd38ff18c6
Cole <eloc49@gmail.com>
2018-09-02 08:29:27 -0600

    rewriting urls for old posts

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const ghost = require('ghost')
+const express = require('express')
+
+const rewritten = require("./routes/rewritten")
+
+const app = express()
 
 ghost().then(ghostServer => {
-  ghostServer.start()
+  rewritten.postRoutes.forEach(route => {
+    let ghostUrl
+    rewritten.postDirs.forEach(dir => {
+      if (route.startsWith(`/${dir}`)) {
+        ghostUrl = route.replace(`/${dir}`, "")
+      }
+    })
+    console.log(ghostUrl)
+    app.get(route, (req, res) => {
+      res.redirect(ghostUrl)
+    })
+  })
+
+  app.use(ghostServer.rootApp)
+  ghostServer.start(app)
 })
+
