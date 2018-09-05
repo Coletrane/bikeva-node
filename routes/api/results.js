6cf6506a88d4f559f497c44e93c80402a56bf60c
Cole <eloc49@gmail.com>
2018-09-05 07:03:38 -0600

    got connected to rds instance, updated results,js, added ssh script

--- routes/api/results.js
+++ routes/api/results.js
@@ -1,30 +1,36 @@
 const router = require("express").Router()
 const mysql = require("mysql")
+
 let config = require("../../config.development")
 if (process.env.NODE_ENV === "production") {
   config = require("../../config.production")
 }
 
 const conn = mysql.createConnection({
-
+  host: config.database.connection.host,
+  user: config.database.connection.user,
+  password: config.database.connection.password,
+  database: config.database.connection.database
 })
+conn.connect()
+
 router.get("/:name", (req, res) => {
   // TODO: add logger
   console.log(`Querying databse for race: ${req.params.name}`)
-  db.all(`SELECT * FROM results WHERE name = '${req.params.name}'`,
-    (err, data) => {
+  conn.query(`SELECT * FROM results WHERE name = '${req.params.name}'`,
+    (err, results, fields) => {
       if (err) {
         res.status(500).send(err)
       }
 
-      if (!data) {
+      if (!results || results.length === 0) {
         res.status(404).send()
-      } else if (data.length > 1) {
+      } else if (results.length > 1) {
         res.status(500).send("Duplicate race names")
       } else {
         console.log(`Results found! ${req.params.name}`)
         res.setHeader("Content-Type", "application/json")
-        res.send(data[0].json)
+        res.send(results[0].json)
       }
     })
 })
---
+++ scripts/ssh-ec2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh -i ~/.ssh/ghost.pem ubuntu@ec2-18-212-57-223.compute-1.amazonaws.com
