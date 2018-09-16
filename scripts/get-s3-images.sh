175383a46a837e01c22150553d4b59b1e1ed660d
Cole <eloc49@gmail.com>
2018-09-16 10:47:10 -0400

    updating production config

--- scripts/get-s3-images.sh
+++ scripts/get-s3-images.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync --delete s3://mtbva-ghost ./content/images
+aws s3 sync --delete s3://mtbva-ghost/content/images ./content/images
--- scripts/put-s3-images.sh
+++ scripts/put-s3-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 sync --delete ./content/images s3://mtbva-ghost --cache-control max-age=2629746  --exclude '*.DS_Store'
-aws cloudfront create-invalidation --distribution-id E1SHPBQLNFW05Q --paths '/*';
+aws s3 sync --delete ./content/images s3://mtbva-ghost/content/images  --exclude '*.DS_Store'
+aws cloudfront create-invalidation --distribution-id E370H18O1A8QJQ  --paths '/content/images*';
